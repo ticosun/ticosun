cea15971d9c8144f482e6e515c56cb103601b6a2
ticosun <ticosun@163.com>
2017-05-24 09:59:47 +0800

    fix

--- switchTab/js/main.js
+++ switchTab/js/main.js
@@ -1,24 +1,21 @@
 /**
- * 引入jQuery库 及 自定义功能模块实现Tab切换
- * Author: Avenstar
- * 2017/2/17
- */
+* 引入jQuery库 及 自定义功能模块实现Tab切换
+* Author: ticosun
+* 2017/5/8
+*/
 require.config({
-	baseUrl : './js/lib/',
-	paths: {
-		'jquery' : 'jquery-1.11.1/jquery.min',  /*引入jQuery库*/
-        'bootstrap' : 'bootstrap-3.3.5/bootstrap.min',
-        'switchTab' : 'component/switchTab' 
-	},
-    /*shim参数中，指定了模块名称和它的依赖数组*/
+    baseUrl: './js/lib/',
+    paths: {
+      'jquery': 'jquery-1.11.1/jquery.min',
+      'bootstrap': 'bootstrap-3.3.5/bootstrap.min',
+      'switchTab': 'component/switchTab'
+    },
     shim: {
-        'bootstrap' : ['jquery']  /*Bootstrap's JavaScript requires jQuery*/
+      'bootstrap': ['jquery']
     }
-})
-//按需加载
-require(['jquery','bootstrap', 'switchTab'], function($, bootstrap, switchTab){
-	$(function(){
-		//初始化
-		switchTab.init();
-	})
+});
+require(['jquery', 'bootstrap', 'switchTab'], function ($, bootstrap, switchTab) {
+    $(function () {
+      switchTab.init()
+    })
 })
